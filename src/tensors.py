1240900f304d211740f5afd6cbbaad156719a8fe
Aaliyah Harper <aeverly14@pm.me>
2025-07-29 13:24:34 -0400

    operations start

--- src/tensors.py
+++ src/tensors.py
@@ -1,6 +1,7 @@
 import numpy as np
 import torch
 
+# Create tensors
 data = [[1, 2], [3, 4]]
 x_data = torch.tensor(data)
 
@@ -13,11 +14,29 @@ print(f"Ones Tensor: \n {x_ones} \n")
 x_rand = torch.rand_like(x_data, dtype=torch.float)
 print(f"Random Tensor: \n {x_rand} \n")
 
+# Shape
 shape = (2, 3)
 rand_tensor = torch.rand(shape)
 ones_tensor = torch.ones(shape)
 zeros_tensor = torch.zeros(shape)
-
 print(f"Random Tensor: \n {rand_tensor} \n")
 print(f"Ones Tensor: \n {ones_tensor} \n")
 print(f"Zeros Tensor: \n {zeros_tensor}")
+
+# Attributes
+tensor = torch.rand(3, 4)
+print(f"Shape of tensor: {tensor.shape}")
+print(f"Datatype of tensor: {tensor.dtype}")
+print(f"Device tensor is stored on: {tensor.device}")
+print()
+
+# Operations
+if torch.accelerator.is_available():
+    tensor = tensor.to(torch.accelerator.current_accelerator())
+
+tensor = torch.ones(4, 4)
+print(f"First row: {tensor[0]}")
+print(f"First column: {tensor[:, 0]}")
+print(f"Last column: {tensor[..., -1]}")
+tensor[:, 1] = 0
+print(tensor)
